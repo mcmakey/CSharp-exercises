2dfff444331f5f40199a7e25dbb0d76278883b31
makey <vadimmakievsky@gmail.com>
2021-02-18 05:27:47 +0300

    Запрос имени и вывод приветствия

---
+++ study_quarter_1/Introduction_csharp/lesson_1/Exercise-1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            string userName = Console.ReadLine();
+            string dateToDisplay = DateTime.Today.ToString("MMMM dd, yyyy, en-US");
+            Console.WriteLine($"Hello {userName} today {dateToDisplay}");
+        }
+    }
+}
