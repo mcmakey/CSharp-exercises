e81f13b99624edd0ca07d9a3418927131ca3224a
makey <vadimmakievsky@gmail.com>
2021-02-18 05:33:01 +0300

    Вывод даты на английском

--- study_quarter_1/Introduction_csharp/lesson_1/Exercise-1/Program.cs
+++ study_quarter_1/Introduction_csharp/lesson_1/Exercise-1/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Введение в C#. Урок 1. Практическое задание 1.
+// Написать программу, выводящую в консоль текст: «Привет, %имя пользователя%, сегодня %дата%».
+// Имя пользователя сохранить из консоли в промежуточную переменную.
+
+using System;
 
 namespace Exercise_1
 {
@@ -6,10 +10,14 @@ namespace Exercise_1
     {
         static void Main(string[] args)
         {
+            var usCulture = new System.Globalization.CultureInfo("en-US");
+
             Console.WriteLine("What's your name?");
+
             string userName = Console.ReadLine();
-            string dateToDisplay = DateTime.Today.ToString("MMMM dd, yyyy, en-US");
-            Console.WriteLine($"Hello {userName} today {dateToDisplay}");
+            string dateToDisplay = DateTime.Today.ToString("MMMM dd, yyyy", usCulture.DateTimeFormat);
+
+            Console.WriteLine($"Hi {userName} today {dateToDisplay}");
         }
     }
 }
