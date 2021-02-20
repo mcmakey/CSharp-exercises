dc291256aec0b85134b01fa8765f8527b85d017b
makey <vadimmakievsky@gmail.com>
2021-02-20 05:55:28 +0300

    init

---
+++ study_quarter_1/Introduction_csharp/lesson_2/ex-2-1/exercise-2-1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Введение в C#. Урок 2. Практическое задание 1.
+// Запросить у пользователя минимальную и максимальную температуру
+// за сутки и вывести среднесуточную температуру.
+
+using System;
+
+namespace exercise_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
