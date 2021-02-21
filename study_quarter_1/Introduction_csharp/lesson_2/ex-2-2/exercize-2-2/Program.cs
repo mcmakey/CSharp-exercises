3ad6da8ff2634b2d9e458336ace3240ad849d278
makey <vadimmakievsky@gmail.com>
2021-02-21 06:15:13 +0300

    Вводи и проверка корректности номера месяца

--- study_quarter_1/Introduction_csharp/lesson_2/ex-2-2/exercize-2-2/Program.cs
+++ study_quarter_1/Introduction_csharp/lesson_2/ex-2-2/exercize-2-2/Program.cs
@@ -1,12 +1,59 @@
-﻿using System;
+﻿// Введение в C#. Урок 2. Практическое задание 2.
+// Запросить у пользователя порядковый номер текущего месяца и вывести его название.
+
+using System;
 
 namespace exercize_2_2
 {
     class Program
     {
+
+        enum Months
+        {
+            January,
+            February,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            November,
+            December
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int getMonthNumber()
+            {
+                string value;
+                int number;
+                bool isNumber;
+                const int MAX_VALUE = 12;
+
+                while(true)
+                {
+                    value = Console.ReadLine();
+                    isNumber = Int32.TryParse(value, out number);
+
+                    if (isNumber && number <= MAX_VALUE)
+                    {
+                        return number;
+                    } 
+                    else
+                    {
+                        Console.WriteLine("Неккоректный ввод, попрбуйте еще раз");
+                    };
+                }
+            }
+
+            Console.WriteLine("Введите номер месяца");
+
+            int monthNumber = getMonthNumber();
+
+            Console.WriteLine(monthNumber);
         }
     }
 }
