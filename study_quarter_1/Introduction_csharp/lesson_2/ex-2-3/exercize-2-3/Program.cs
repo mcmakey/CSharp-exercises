70e5e363d8373d9a9c7d4654f9543300449dbea0
makey <vadimmakievsky@gmail.com>
2021-02-21 07:29:07 +0300

    Проверка на четность и нечетность

--- study_quarter_1/Introduction_csharp/lesson_2/ex-2-3/exercize-2-3/Program.cs
+++ study_quarter_1/Introduction_csharp/lesson_2/ex-2-3/exercize-2-3/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Введение в C#. Урок 2. Практическое задание 3.
 // Определить, является ли введённое пользователем число чётным.
+
 using System;
 
 namespace exercize_2_3
@@ -8,7 +9,19 @@ namespace exercize_2_3
     {
         static void Main(string[] args)
         {
-            // Определить, является ли введённое пользователем число чётным.
+
+            int number;
+
+            Console.WriteLine("Введите чило");
+
+            if (Int32.TryParse(Console.ReadLine(), out number) && number % 2 == 0 )
+            {
+                Console.WriteLine("Число четное");
+            }
+            else
+            {
+                Console.WriteLine("Число не четное");
+            }
         }
     }
 }
