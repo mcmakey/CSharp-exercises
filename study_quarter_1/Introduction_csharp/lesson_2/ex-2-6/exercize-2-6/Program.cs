30a5df77667e7e3e4901e683a6ed7daaf6647295
makey <vadimmakievsky@gmail.com>
2021-02-22 16:30:23 +0300

    Отображение дней работы офисов

--- study_quarter_1/Introduction_csharp/lesson_2/ex-2-6/exercize-2-6/Program.cs
+++ study_quarter_1/Introduction_csharp/lesson_2/ex-2-6/exercize-2-6/Program.cs
@@ -11,12 +11,41 @@ namespace exercize_2_6
 {
     class Program
     {
+        [Flags]
+        enum DaysOfWeek
+        {
+            Monday    = 0b_0000001,
+            Tuesday   = 0b_0000010,
+            Wednesday = 0b_0000100,
+            Thursday  = 0b_0001000,
+            Friday    = 0b_0010000,
+            Saturday  = 0b_0100000,
+            Sunday    = 0b_1000000,
+        }
         static void Main(string[] args)
         {
-            // Для полного закрепления битовых масок, попытайтесь создать универсальную структуру
-            // расписания недели, к примеру, чтобы описать работу какого либо офиса.
-            // Явный пример - офис номер 1 работает со вторника до пятницы, офис номер 2 работает
-            // с понедельника до воскресенья и выведите его на экран консоли.
+
+            DaysOfWeek workDaysOfficeOne =
+                DaysOfWeek.Monday |
+                DaysOfWeek.Tuesday |
+                DaysOfWeek.Wednesday |
+                DaysOfWeek.Thursday |
+                DaysOfWeek.Friday |
+                DaysOfWeek.Saturday |
+                DaysOfWeek.Sunday;
+
+            DaysOfWeek workDaysOfficeTwo =
+                DaysOfWeek.Saturday |
+                DaysOfWeek.Sunday;
+
+            DaysOfWeek workDaysOfficeThree =
+                DaysOfWeek.Monday |
+                DaysOfWeek.Wednesday |
+                DaysOfWeek.Friday;
+
+            Console.WriteLine($"Working days of office 1: {workDaysOfficeOne}");
+            Console.WriteLine($"Working days of office 2: {workDaysOfficeTwo}");
+            Console.WriteLine($"Working days of office 3: {workDaysOfficeThree}");
         }
     }
 }
