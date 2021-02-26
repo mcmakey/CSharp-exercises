de7271916b648812efbd0acb4bb52e6ba0ea0a7f
makey <vadimmakievsky@gmail.com>
2021-02-26 07:09:03 +0300

    Вывод членов главно диагонали матрицы

--- study_quarter_1/Introduction_csharp/lesson_3/Exercise-3-1/Program.cs
+++ study_quarter_1/Introduction_csharp/lesson_3/Exercise-3-1/Program.cs
@@ -9,7 +9,64 @@ namespace Exercise_3_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Написать программу, выводящую элементы двухмерного массива по диагонали.");
+            int[,] myMatrix = new int[4, 5] { 
+                { 1, 2, 3, 4, 5 },
+                { 6, 7, 8, 9, 10 },
+                { 11, 12, 13, 14, 15 },
+                { 16, 17, 18, 19, 20}
+            };
+           
+            ShowMatrix(myMatrix);
+            ShowMembersMainDiagonal(GetMembersMainDiagonal(myMatrix));
+        }
+
+        static void ShowMatrix(int[,] matrix)
+        {
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+
+            Console.WriteLine("Дан двумерный массив: ");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{matrix[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void ShowMembersMainDiagonal(int[] array)
+        {
+            Console.WriteLine($"Элементы данного двумерного массива по главной диагонали: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]}\t");
+            }
+        }
+
+        static int[] GetMembersMainDiagonal(int [,] matrix)
+        {
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+
+            int numberMembers = rows > columns ? columns : rows;
+
+            int[] membersMainDiagonal = new int[numberMembers];
+
+            for (int i = 0; i < membersMainDiagonal.Length; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if ( i == j)
+                    {
+                        membersMainDiagonal[i] = matrix[i, j];
+                    }
+                }
+            }
+
+            return membersMainDiagonal;
         }
     }
 }
