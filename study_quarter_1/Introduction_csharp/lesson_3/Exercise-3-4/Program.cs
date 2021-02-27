fc012ef54041a8539ede940972f4f32c33bc2689
makey <vadimmakievsky@gmail.com>
2021-02-27 15:02:59 +0300

    Отображение игрового поля

--- study_quarter_1/Introduction_csharp/lesson_3/Exercise-3-4/Program.cs
+++ study_quarter_1/Introduction_csharp/lesson_3/Exercise-3-4/Program.cs
@@ -10,7 +10,55 @@ namespace Exercise_3_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("«Морской бой»");
+
+            // Инициализация игрового поля
+            string[,] battleField = InitBattleField();
+
+            // Показать пустое игровое поле
+            ShowBattleField(battleField);
         }
+
+        static string[,] InitBattleField()
+        {
+            string[,] matrix = new string[10, 10];
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = "O";
+                }
+            }
+
+            return matrix;
+        }
+
+        static void ShowBattleField(string[,] matrix)
+        {
+            string[] xСoordinateNames = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+
+            for (int i = 0; i < xСoordinateNames.Length; i++)
+            {
+                Console.Write(xСoordinateNames[i].PadLeft(i == 0 ? 4 : 2));
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < rows; i++)
+            {
+                Console.Write($"{i + 1}".PadLeft(2));
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(matrix[i, j].PadLeft(2));
+                }
+                Console.WriteLine();
+            }
+        }
+
     }
 }
