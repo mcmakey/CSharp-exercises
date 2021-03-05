54245f69fda0b9be086cf220ecd9270e499aa256
makey <vadimmakievsky@gmail.com>
2021-03-05 06:48:51 +0300

    Вывод в консоль ФИО через метод GetFullName

---
+++ study_quarter_1/Introduction_csharp/lesson_4/Exercise-4-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise_4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetFullName("Амбросий", "Суслопаров", "Викторович"));
+            Console.WriteLine(GetFullName("Мартын", "Трегубов", "Аскольдович"));
+            Console.WriteLine(GetFullName("Иннокентий ", "Виноградов", "Савельевич"));
+        }
+
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            return $"{lastName} {firstName} {patronymic}";
+        }
+    }
+}
