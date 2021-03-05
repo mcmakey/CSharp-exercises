704d1ef61a33f45ebc099bb021dc233e9701bdfe
makey <vadimmakievsky@gmail.com>
2021-03-05 22:33:05 +0300

    Ex-4-4

--- study_quarter_1/Introduction_csharp/lesson_4/Exercise-4-4/Program.cs
+++ study_quarter_1/Introduction_csharp/lesson_4/Exercise-4-4/Program.cs
@@ -9,7 +9,38 @@ namespace Exercise_4_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Написать программу, вычисляющую число Фибоначчи для заданного значения рекурсивным способом.");
+            Console.WriteLine("Введите целое число n для вычисления n-ого числа Фибоначи");
+            int n = EnterdNumber();
+            Console.WriteLine($"{n}-ое число Фибоначи это {GetFibonaci(n)}");
+        }
+
+        static int EnterdNumber()
+        {
+            while(true)
+            {
+                if(Int32.TryParse(Console.ReadLine(), out int n))
+                {
+                    return n;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод, попробуйте еще");
+                }
+            }
+        }
+
+        static int GetFibonaci(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+
+            return GetFibonaci(n - 2) + GetFibonaci(n - 1);
         }
     }
 }
