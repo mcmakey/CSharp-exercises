792a37871056f1fb4f3ceeea8a6fbd1374d90301
makey <vadimmakievsky@gmail.com>
2021-03-05 15:22:19 +0300

    Ex-4-3: Почти

--- study_quarter_1/Introduction_csharp/lesson_4/Exercise-4-3/Program.cs
+++ study_quarter_1/Introduction_csharp/lesson_4/Exercise-4-3/Program.cs
@@ -11,9 +11,81 @@ namespace Exercise_4_3
 {
     class Program
     {
+        enum Seasons
+        {
+            Winter,
+            Spring,
+            Summer,
+            Autumn
+        };
+
+        const int MAX_NUMBER_MONTH = 12;
+        const int MIN_NUMBER_MONTH = 1;
+        const int NUMBER_MONTH_SEASON = 3;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Написать метод по определению времени года");
+            Console.WriteLine("Введите номер месяца");
+            int numberMonth = EnteredNumberMonth();
+            string season = GetSeason(numberMonth);
+
+        }
+
+        static int EnteredNumberMonth()
+        {
+            while(true)
+            {
+                if( Int32.TryParse(Console.ReadLine(), out int number) && 
+                    (MIN_NUMBER_MONTH <= number && number <= MAX_NUMBER_MONTH)
+                  )
+                {
+                    return number;
+                } else
+                {
+                    Console.WriteLine("Ошибка: введите число от 1 до 12");
+                }
+            }
+        }
+
+        static string GetSeason(int numberMonth)
+        {
+            int indexOfSeason = numberMonth / NUMBER_MONTH_SEASON;
+            if (indexOfSeason == MAX_NUMBER_MONTH / NUMBER_MONTH_SEASON)
+            {
+                indexOfSeason = 0;
+            }
+
+            return Enum.GetName(typeof(Seasons), indexOfSeason);
+        }
+
+        static string GetSeasonName(string season)
+        {
+            string seasonName;
+
+            if (Enum.TryParse(season, out Seasons value))
+            {
+                switch (value)
+                {
+                    case Seasons.Winter:
+                        seasonName = "Зима";
+                        break;
+                    case Seasons.Spring:
+                        seasonName = "Весна";
+                        break;
+                    case Seasons.Summer:
+                        seasonName = "Лето";
+                        break;
+                    case Seasons.Autumn:
+                        seasonName = "Осень";
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка");
+            }
+
+            return seasonName; 
         }
     }
 }
