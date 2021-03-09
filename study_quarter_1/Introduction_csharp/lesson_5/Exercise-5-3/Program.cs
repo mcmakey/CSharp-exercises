cbe1f93670898a7bd7af941ddb6d10eb384a0379
makey <vadimmakievsky@gmail.com>
2021-03-09 07:37:46 +0300

    Ex-5-3: Набор чисел в бинарник (хардкод)

--- study_quarter_1/Introduction_csharp/lesson_5/Exercise-5-3/Program.cs
+++ study_quarter_1/Introduction_csharp/lesson_5/Exercise-5-3/Program.cs
@@ -2,6 +2,7 @@
 // Ввести с клавиатуры произвольный набор чисел (0...255) и записать их в бинарный файл.
 
 using System;
+using System.IO;
 
 namespace Exercise_5_3
 {
@@ -9,7 +10,23 @@ namespace Exercise_5_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ввести с клавиатуры произвольный набор чисел (0...255) и записать их в бинарный файл.");
+            string path = @"C:\temp";
+
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+
+
+            byte[] numbers = new byte[] { 0, 10, 30, 60, 100, 200, 255, 120, 35, 10, 56, 244 };
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(@$"{path}\test.bin", FileMode.OpenOrCreate)))
+            {
+                writer.Write(numbers);
+            }
+
         }
     }
 }
