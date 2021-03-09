446965a76feb3d8f8c8caa535f4651ec638df7c8
makey <vadimmakievsky@gmail.com>
2021-03-09 12:15:57 +0300

    Ex-5-3: Ввод набора чисел с клавиатуры + валидация

--- study_quarter_1/Introduction_csharp/lesson_5/Exercise-5-3/Program.cs
+++ study_quarter_1/Introduction_csharp/lesson_5/Exercise-5-3/Program.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Exercise_5_3
 {
@@ -20,13 +21,49 @@ namespace Exercise_5_3
             }
 
 
-            byte[] numbers = new byte[] { 0, 10, 30, 60, 100, 200, 255, 120, 35, 10, 56, 244 };
+            byte[] numbers = GetNumbers();
 
             using (BinaryWriter writer = new BinaryWriter(File.Open(@$"{path}\test.bin", FileMode.OpenOrCreate)))
             {
                 writer.Write(numbers);
             }
 
+            Console.WriteLine(@"Результаты записаны в файл C:\temp\test.bin");
+        }
+
+        static byte[] GetNumbers()
+        {
+            string delimiter = "[,]+";
+            Regex template = new Regex(@"^[0-9,]+$");
+            Console.WriteLine("Введите произвольный набор чисел от 0 до 255, разделенные запятой");
+
+            while (true)
+            {
+                string entered = Console.ReadLine();
+                if (template.IsMatch(entered))
+                {
+                    string[] numbers = Regex.Split(entered, delimiter);
+
+                    if (Array.TrueForAll(numbers, CheckToByte))
+                    {
+                        return Array.ConvertAll(numbers, byte.Parse);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Есть числа не входящие в диапазон 0-255");
+                    };
+                }
+                else
+                {
+                    Console.WriteLine("Введенная строка содержит символы отличающиеся от цифр и запятых");
+                }
+                Console.WriteLine("Попробуйте еще раз");
+            }
+        }
+
+        static bool CheckToByte(string value)
+        {
+            return 0 <=  Convert.ToInt32(value) && Convert.ToInt32(value) <= 255;
         }
     }
 }
