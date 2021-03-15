814874050e504ff8bf71ea58b942c14e7fa3065c
makey <vadimmakievsky@gmail.com>
2021-03-15 09:39:37 +0300

    Ex-7-1: Декомпилированная сборка импортирована в проект Exercise-7-1-restore

---
+++ study_quarter_1/Introduction_csharp/lesson_7/Exercise-7-1-restore/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Exercise-7-1")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("Exercise-7-1")]
+[assembly: AssemblyTitle("Exercise-7-1")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ study_quarter_1/Introduction_csharp/lesson_7/Exercise-7-1-restore/Square.cs
@@ -0,0 +1,17 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Exercise_7_1.Square
+// Assembly: Exercise-7-1, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: C112FAA2-EEAC-4BBF-B131-1798553B6F00
+// Assembly location: D:\vav\star\2\w\learning\training\c#\geekbrains_kurs\code\homework\geekbrains_csharp_homework\study_quarter_1\Introduction_csharp\lesson_7\Exercise-7-1\bin\Debug\netcoreapp3.1\Exercise-7-1.dll
+
+namespace Exercise_7_1
+{
+  internal class Square
+  {
+    public float side { get; set; }
+
+    public Square(float side) => this.side = side;
+
+    public float GetPerimeter() => this.side * 4f;
+  }
+}
