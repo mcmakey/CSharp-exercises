9362bebab2370d18ef85503b8b87c713598f8f67
makey <vadimmakievsky@gmail.com>
2021-03-15 08:47:29 +0300

    Revert "Ex-7-1: Удаление исходников"
    This reverts commit 1bba041efc457162553c9606a1e926a62993fb4b.

---
+++ study_quarter_1/Introduction_csharp/lesson_7/Exercise-7-1/Square.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise_7_1
+{
+    class Square
+    {
+        public float side { get; set; }
+
+        public Square(float side)
+        {
+            this.side = side;
+        }
+
+        public float GetPerimeter()
+        {
+            return side * 4;
+        }
+    }
+}
