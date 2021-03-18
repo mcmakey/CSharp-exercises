b6129ab79b065b344c9978491334c10164aeed0f
makey <vadimmakievsky@gmail.com>
2021-03-18 06:55:22 +0300

    Ex-8-1: Вывод ранее сохраненных данных пользователя в настройках приложения

--- study_quarter_1/Introduction_csharp/lesson_8/Exercise-8-1/Program.cs
+++ study_quarter_1/Introduction_csharp/lesson_8/Exercise-8-1/Program.cs
@@ -12,16 +12,23 @@ namespace Exercise_8_1
 {
     class Program
     {
+        const string APP_SETTING_GREETING_KEY = "greeting";
+        const string APP_SETTING_USER_NAME_KEY = "userName";
+        const string APP_SETTING_USER_AGE_KEY = "userAge";
+        const string APP_SETTING_USER_ACTIVITIES_KEY = "userActivities";
+
         static void Main(string[] args)
         {
+
             Greeting();
+
+            DisplayAppSettings();
 
             (string name, string age, string activities) user = GetPersonInfo();
             
-
-            UpdateAppSettings("userName", user.name);
-            UpdateAppSettings("userAge", user.age);
-            UpdateAppSettings("userActivities", user.activities);
+            UpdateAppSettings(APP_SETTING_USER_NAME_KEY, user.name);
+            UpdateAppSettings(APP_SETTING_USER_AGE_KEY, user.age);
+            UpdateAppSettings(APP_SETTING_USER_ACTIVITIES_KEY, user.activities);
         }
 
         static void Greeting()
@@ -29,7 +36,8 @@ namespace Exercise_8_1
             try
             {
                 var appSettings = ConfigurationManager.AppSettings;
-                Console.WriteLine(appSettings["greeting"]);
+                Console.WriteLine(appSettings[APP_SETTING_GREETING_KEY]);
+                Console.WriteLine();
             }
             catch (ConfigurationErrorsException)
             {
@@ -69,6 +77,28 @@ namespace Exercise_8_1
 
                 configFile.Save(ConfigurationSaveMode.Modified);
                 ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine($"Произошла ошибка записи настроек");
+            }
+        }
+
+        static void DisplayAppSettings()
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+
+                foreach (var key in appSettings.AllKeys)
+                {
+                    if ( key != APP_SETTING_GREETING_KEY)
+                    {
+                        Console.Write($"{appSettings[key]} ");
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine();
             }
             catch (ConfigurationErrorsException)
             {
