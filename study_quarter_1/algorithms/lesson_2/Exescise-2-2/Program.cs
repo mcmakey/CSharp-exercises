6b5f859540696e7d89189ac133597d4f3033641d
makey <vadimmakievsky@gmail.com>
2021-04-23 15:17:42 +0300

    Ex-2-2: Функция бинарного поиска и тестирование ее

--- study_quarter_1/algorithms/lesson_2/Exescise-2-2/Program.cs
+++ study_quarter_1/algorithms/lesson_2/Exescise-2-2/Program.cs
@@ -6,11 +6,92 @@ using System;
 
 namespace Exescise_2_2
 {
+    
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("функция бинарного поиска");
+            // positive
+            TestCase testCase1 = new TestCase()
+            {
+                Array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+                Value = 4,
+                Expected = 3
+            };
+
+            TestCase testCase2 = new TestCase()
+            {
+                Array = new int[] { 4, 7, 10, 18, 19, 35, 90, 123, 456, 10000 },
+                Value = 123,
+                Expected = 7
+            };
+
+            TestCase testCase3 = new TestCase()
+            {
+                Array = new int[] { 4, 7, 10, 18, 19, 35, 90, 123, 456, 10000 },
+                Value = 33,
+                Expected = -1
+            };
+
+            // negative
+            TestCase testCase4 = new TestCase()
+            {
+                Array = new int[] { 4, 7, 10, 18, 19, 35, 90, 123, 456, 10000 },
+                Value = 18,
+                Expected = 4
+            };
+
+            TestBynarySearch(testCase1);
+            TestBynarySearch(testCase2);
+            TestBynarySearch(testCase3);
+            TestBynarySearch(testCase4);
+        }
+
+        public class TestCase
+        {
+            public int[] Array { get; set; }
+            public int Value { get; set; }
+            public int Expected { get; set; }
+        }
+
+        public static void TestBynarySearch(TestCase testCase)
+        {
+            var actual = BynarySearch(testCase.Array, testCase.Value);
+            
+            if (actual == testCase.Expected)
+            {
+                Console.WriteLine("VALID TEST");
+            } 
+            else
+            {
+                Console.WriteLine("INVALID TEST");
+            }
+        }
+
+        static int BynarySearch(int[] array, int value)
+        {
+            var minIndex = 0;
+            var maxIndex = array.Length - 1;
+
+            while (minIndex <= maxIndex)
+            {
+                int midIndex = (minIndex + maxIndex) / 2;
+                if (value == array[midIndex])
+                {
+                    return midIndex;
+                }
+                else if (value < array[midIndex])
+                {
+                    maxIndex = midIndex - 1;
+                }
+                else
+                {
+                    minIndex = midIndex + 1;
+                }
+            }
+
+            return -1;
         }
     }
 }
