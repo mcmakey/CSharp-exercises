a3ef9d772af86c73df903fcbdbb509385d38cc4c
makey <vadimmakievsky@gmail.com>
2021-05-03 08:33:20 +0300

    Ex-5-1:  initial commit

---
+++ study_quarter_1/algorithms/lesson_5/Exescise-5-1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Алгоритмы. Урок 1. Задание 1
+// Реализуйте DFS и BFS для дерева с выводом каждого шага в консоль
+
+using System;
+
+namespace Exescise_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DFS и BFS для дерева");
+        }
+    }
+}
