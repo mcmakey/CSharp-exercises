f69bc4f04981bc57b41619126b438d9574f71a68
makey <vadimmakievsky@gmail.com>
2021-05-09 08:33:46 +0300

    Ex-7-1:  Классы рюкзака и вещей

---
+++ study_quarter_1/algorithms/lesson_7/Exercise-7-1/Backpack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_7_1
+{
+    class Backpack
+    {
+        public int Capacity { get; }
+
+        public Backpack(int capacity)
+        {
+            this.Capacity = capacity;
+        }
+    }
+}
---
+++ study_quarter_1/algorithms/lesson_7/Exercise-7-1/Thing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_7_1
+{
+    class Thing
+    {
+        public string Name { get; }
+        public int Weight { get; }
+        public int Utility { get; }
+
+        public Thing(string name, int weight, int utility)
+        {
+            this.Name = name;
+            this.Weight = weight;
+            this.Utility = utility;
+        }
+    }
+}
