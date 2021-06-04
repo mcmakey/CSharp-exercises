de0cd1037f8105aa828268aad713dfbf78cf8f6e
makey <vadimmakievsky@gmail.com>
2021-06-04 14:14:09 +0300

    Ex-1: Созданны классы для записи и списка записей

---
+++ study_quarter_2/asp-net-web-api/lesson_1/Exercise-1/WeatherDiary.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Exercise_1
+{
+    public class WeatherDiary
+    {
+        public List<WeatherDiaryEntry> Entries { get; set; }
+
+        public WeatherDiary()
+        {
+            Entries = new List<WeatherDiaryEntry> { };
+        }
+    }
+}
---
+++ study_quarter_2/asp-net-web-api/lesson_1/Exercise-1/WeatherDiaryEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    public class WeatherDiaryEntry
+    {
+        private DateTime _time;
+        private sbyte _temperature;
+
+        public WeatherDiaryEntry(DateTime time, sbyte temperature)
+        {
+            _time = time;
+            _temperature = temperature;
+        }
+
+        public DateTime Time
+        {
+            get => _time;
+        }
+
+        public sbyte Temperature
+        {
+            get => _temperature;
+        }
+    }
+}
