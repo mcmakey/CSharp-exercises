3333d6a043a58fc016828aa41f41c58a0c9ad3e7
makey <vadimmakievsky@gmail.com>
2021-06-04 10:03:16 +0300

    Ex-1: example "CrudController"

---
+++ study_quarter_2/asp-net-web-api/lesson_1/Exercise-1/ValuesHolder.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Exercise_1
+{
+    public class ValuesHolder
+    {
+        public List<string> Values { get; set; }
+
+        public ValuesHolder()
+        {
+            Values = new List<string> { };
+        }
+    }
+}
