b35666c1c014301d897790ddc423ee455b92ec3d
makey <vadimmakievsky@gmail.com>
2021-06-04 10:11:26 +0300

    Ex-1: delete default template "weatherforecast"

--- study_quarter_2/asp-net-web-api/lesson_1/Exercise-1/Startup.cs
+++ study_quarter_2/asp-net-web-api/lesson_1/Exercise-1/Startup.cs
@@ -1,16 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Exercise_1
 {
@@ -28,11 +21,6 @@ namespace Exercise_1
         {
             services.AddSingleton<ValuesHolder>();
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Exercise_1", Version = "v1" });
-            });
-            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,8 +29,6 @@ namespace Exercise_1
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Exercise_1 v1"));
             }
 
             app.UseHttpsRedirection();
