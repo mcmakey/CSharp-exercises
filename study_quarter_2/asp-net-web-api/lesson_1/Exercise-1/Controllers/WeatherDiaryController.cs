e2cda6f4b06299be4226c62c786054bd0959ae2d
makey <vadimmakievsky@gmail.com>
2021-06-05 16:52:39 +0300

    Ex-1: удален закоментрованный код

--- study_quarter_2/asp-net-web-api/lesson_1/Exercise-1/Controllers/WeatherDiaryController.cs
+++ study_quarter_2/asp-net-web-api/lesson_1/Exercise-1/Controllers/WeatherDiaryController.cs
@@ -32,13 +32,6 @@ namespace Exercise_1.Controllers
             return BadRequest("невалидные данные");
         }
 
-        // TODO: for dev
-        //[HttpGet("read")]
-        //public IActionResult Read()
-        //{
-        //    return Ok(_weatherDiary.Entries);
-        //}
-
         [HttpGet("readbytimeinterval")]
         public IActionResult ReadByTimeInterval([FromQuery] string start, [FromQuery] string end)
         {
