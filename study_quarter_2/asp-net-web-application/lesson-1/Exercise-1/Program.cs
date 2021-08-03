f7dabaf54f1226a57ead70041aefb2062c86ab5e
makey <vadimmakievsky@gmail.com>
2021-08-03 08:38:27 +0300

    Ex-1: Получение постов

--- study_quarter_2/asp-net-web-application/lesson-1/Exercise-1/Program.cs
+++ study_quarter_2/asp-net-web-application/lesson-1/Exercise-1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Exercise_1
@@ -9,23 +10,42 @@ namespace Exercise_1
     class Program
     {
         static readonly HttpClient client = new HttpClient();
-        static void Main(string[] args)
+        static readonly string url = "https://jsonplaceholder.typicode.com/posts/";
+        static async Task Main(string[] args)
         {
-            int[] postIds = { 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+            int[] postIds = { 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
             
-            var posts = GetPostAsync(postIds);
+            var posts = await GetPostAsync(postIds);
+
+            foreach (var post in posts)
+            {
+                Console.WriteLine($"{post.UserId}");
+                Console.WriteLine($"{post.Id}");
+                Console.WriteLine($"{post.Title}");
+                Console.WriteLine($"{post.Body}");
+                Console.WriteLine();
+            }
         }
 
-        public static async Task<Post> GetPostAsync(int postId)
+        public static async Task<Post> GetPostAsync(int id)
         {
-            HttpResponseMessage response = await client.GetAsync("http://www.contoso.com/");
+            var path = $"{url}{id}";
 
-            return null;
+            var response = await client.GetStringAsync(path);
+
+            var serializeOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            var post = JsonSerializer.Deserialize<Post>(response, serializeOptions);
+
+            return post;
         }
 
-        public static async Task<Post[]> GetPostAsync(IEnumerable<int> postIds)
+        public static async Task<Post[]> GetPostAsync(IEnumerable<int> ids)
         {
-            var getPosts = postIds.Select(id => GetPostAsync(id));
+            var getPosts = ids.Select(id => GetPostAsync(id));
             return await Task.WhenAll(getPosts);
         }
     }
