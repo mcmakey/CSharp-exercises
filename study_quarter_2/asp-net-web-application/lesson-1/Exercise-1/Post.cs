b99c4d16a24acfe3aa9394fc0c37851614037a6e
makey <vadimmakievsky@gmail.com>
2021-08-03 06:25:00 +0300

    Ex-1: Асинхронные задачи укрупненно

---
+++ study_quarter_2/asp-net-web-application/lesson-1/Exercise-1/Post.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise_1
+{
+    class Post
+    {
+       public int UserId { get; set; }
+       public int Id { get; set; }
+       public string Title { get; set; }
+       public string Body { get; set; }
+    }
+}
--- study_quarter_2/asp-net-web-application/lesson-1/Exercise-1/Program.cs
+++ study_quarter_2/asp-net-web-application/lesson-1/Exercise-1/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Exercise_1
 {
     class Program
     {
+        static readonly HttpClient client = new HttpClient();
         static void Main(string[] args)
         {
-            Console.WriteLine("async");
+            int[] postIds = { 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+            
+            var posts = GetPostAsync(postIds);
+        }
+
+        public static async Task<Post> GetPostAsync(int postId)
+        {
+            HttpResponseMessage response = await client.GetAsync("http://www.contoso.com/");
+
+            return null;
+        }
+
+        public static async Task<Post[]> GetPostAsync(IEnumerable<int> postIds)
+        {
+            var getPosts = postIds.Select(id => GetPostAsync(id));
+            return await Task.WhenAll(getPosts);
         }
     }
 }
