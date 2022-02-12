f9fee663ec8aca582daef64036994bacf879159e
makey <vadimmakievsky@gmail.com>
2022-02-12 19:32:02 +0300

    lesson-1: initial commit crud web-api

---
+++ study_quarter_4/secure_development/CardService/Models/Card.cs
@@ -0,0 +1,12 @@
+﻿namespace CardService.Models
+{
+    public class Card
+    {
+        public long Id { get; set; }
+        public long Number { get; set; }
+        public string PaymentSystem { get; set; } = String.Empty;
+        public string Bank { get; set; } = String.Empty;
+        public DateTime ValidThru { get; set; }
+        public string CardOwner { get; set; } = String.Empty;
+    }
+}
---
+++ study_quarter_4/secure_development/CardService/Models/CardServiceContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CardService.Models
+{
+    public class CardServiceContext : DbContext
+    {
+
+        public CardServiceContext(DbContextOptions<CardServiceContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Card> Cards { get; set; } = null!;
+    }
+}
