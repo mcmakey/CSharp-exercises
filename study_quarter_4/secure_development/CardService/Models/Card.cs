502eb4dd8674c6fc3318ba3cadf46911489e2fe6
makey <vadimmakievsky@gmail.com>
2022-02-12 20:18:36 +0300

    lesson-1: уточнение полей класса card

--- study_quarter_4/secure_development/CardService/Models/Card.cs
+++ study_quarter_4/secure_development/CardService/Models/Card.cs
@@ -3,10 +3,11 @@
     public class Card
     {
         public long Id { get; set; }
-        public long Number { get; set; }
-        public string PaymentSystem { get; set; } = String.Empty;
+        public string Number { get; set; } = String.Empty;
+        public PaymentSystem PaymentSystem { get; set; }
         public string Bank { get; set; } = String.Empty;
         public DateTime ValidThru { get; set; }
-        public string CardOwner { get; set; } = String.Empty;
+        public string CardOwnerFirstName { get; set; } = string.Empty;
+        public string CardOwnerLastName { get; set; } = string.Empty;
     }
 }
