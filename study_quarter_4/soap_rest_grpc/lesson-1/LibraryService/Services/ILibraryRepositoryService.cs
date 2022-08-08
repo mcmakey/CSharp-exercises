a604c3338278d8d26835e17bb9b5390dcff308f9
makey <vadimmakievsky@gmail.com>
2022-08-08 14:59:58 +0300

    Lesson-1: Создание LibraryService и добавление в него некоторых методов

---
+++ study_quarter_4/soap_rest_grpc/lesson-1/LibraryService/Services/ILibraryRepositoryService.cs
@@ -0,0 +1,19 @@
+﻿using LibraryService.Models;
+using LibraryService.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryService
+{
+    public interface ILibraryRepositoryService : IRepository<Book>
+    {
+        IList<Book> GetByTitle(string title);
+
+        IList<Book> GetByAuthor(string author);
+
+        IList<Book> GetByCategory(string category);
+    }
+}
---
+++ study_quarter_4/soap_rest_grpc/lesson-1/LibraryService/Services/Impl/LibraryRepository.cs
@@ -0,0 +1,63 @@
+﻿using LibraryService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryService.Services.Impl
+{
+    public class LibraryRepository : ILibraryRepositoryService
+    {
+        private readonly ILibraryDatabaseContextService _dbContext;
+
+        public LibraryRepository(ILibraryDatabaseContextService dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IList<Book> GetByTitle(string title)
+        {
+            return _dbContext.Books.Where(book => book.Title.ToLower().Contains(title.ToLower())).ToList();
+        }
+
+        public IList<Book> GetByAuthor(string authorName)
+        {
+            return _dbContext.Books.Where(book =>
+                book.Authors.Where(author =>
+                    author.Name.Contains(authorName.ToLower())).Count() > 0).ToList();
+        }
+
+        public IList<Book> GetByCategory(string category)
+        {
+            return _dbContext.Books.Where(book =>
+                book.Category.ToLower().Contains(category.ToLower())).ToList();
+        }
+
+        //
+
+        public int? Add(Book item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(Book item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Book> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(Book item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
