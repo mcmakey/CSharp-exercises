d9bc678eaeb28d51647d722e98d1586503a4b424
makey <vadimmakievsky@gmail.com>
2022-08-12 10:59:23 +0300

    Lesson-2: PumpService - основные интерфейсы 51:43

---
+++ study_quarter_4/soap_rest_grpc/lesson-2/PumpService/PumpService/IPumpService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PumpService
+{
+    [ServiceContract(SessionMode = SessionMode.Required)]
+    public interface IPumpService
+    {
+        [OperationContract(IsOneWay = true)]
+        void RunScript();
+
+        [OperationContract(IsOneWay = true)]
+        void UpdateAndcompileScript(string fileName);
+
+        [OperationContract(IsOneWay = true)]
+        void CompileScript();
+    }
+}
