058c29c17bc92900e59cc28f769c69c37066c85b
makey <vadimmakievsky@gmail.com>
2022-08-14 16:02:42 +0300

    Cl-s-lesson-3: Структура бд (классы)

---
+++ study_quarter_4/soap_rest_grpc/lesson-3-n/ClinicService/ClinicService.Data/Pet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicService.Data
+{
+    [Table("Pets")]
+
+    public class Pet
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PetId { get; set; }
+
+        [ForeignKey(nameof(Client))]
+        public int ClientId { get; set; }
+
+        [Column]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        [Column]
+        public DateTime Birthday { get; set; }
+
+        public virtual Client Client { get; set; }
+
+        public ICollection<Consultation> Consultation { get; set; } = new HashSet<Consultation>();
+    }
+}
