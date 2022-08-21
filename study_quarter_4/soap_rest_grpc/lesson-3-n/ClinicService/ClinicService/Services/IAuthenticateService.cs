df4fb5c15d6f56d0cddb9742a2c4f2760a37c15a
makey <vadimmakievsky@gmail.com>
2022-08-21 12:05:22 +0300

    Cl-s-lesson-4: service for autentification

---
+++ study_quarter_4/soap_rest_grpc/lesson-3-n/ClinicService/ClinicService/Services/IAuthenticateService.cs
@@ -0,0 +1,12 @@
+﻿using ClinicService.Models;
+using ClinicService.Models.Requests;
+
+namespace ClinicService.Services
+{
+    public interface IAuthenticateService
+    {
+        AuthenticationResponse Login(AuthenticationRequest authenticationRequest);
+
+        public SessionInfo GetSessionInfo(string SessionToken);
+    }
+}
